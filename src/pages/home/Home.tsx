50c3c6ed89d0705c80681284d829a377f1991a07
devendra2002 <pateldevendra3131@gmail.com>
2024-10-06 23:47:49 +0530

    update file structuer

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import Footer1 from "../../components/footer.tsx/Footer1"
+
+const Home = () => {
+  return (
+    <div>
+       Home real website
+       <Footer1/>
+    </div>
+  )
+}
+
+export default Home
